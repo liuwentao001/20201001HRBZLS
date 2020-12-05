5d4486653de6087d8e259deb2677c8762b5b31ac
lwt <63646955@qq.com>
2020-12-05 09:05:14 +0800

    20201205

---
+++ 02.测试SQL.sql
@@ -0,0 +1,62 @@
+﻿--查询所有用户表
+select * from user_tables
+--查询所有用户表字段
+select * from user_tab_columns order by table_name,column_id
+--查询所有用户表字段备注
+select * from user_col_comments;
+--查询所有用户表备注
+select * from user_tab_comments;
+--查询所有同义词
+SELECT * FROM SYS.ALL_SYNONYMS t WHERE t.owner in ('YYSF')
+--正则表达式
+select case when regexp_like( 'T0101','^T[0-9]*$') then 1 else 0 end from dual
+
+
+--查询所有表说明、字段说明
+select case when regexp_like(user_tab_columns.table_name,'^T[0-9]*$') then '大经理专用' else '外包' end 性质,
+       user_tab_columns.table_name 表名,
+       user_tab_comments.comments 表说明,
+       user_tab_columns.column_name 字段名,
+       user_col_comments.comments 字段说明
+from user_tab_columns 
+     left join user_tab_comments on user_tab_columns.table_name=user_tab_comments.table_name 
+     left join user_col_comments on user_tab_columns.column_name=user_col_comments.column_name and user_tab_columns.table_name=user_col_comments.table_name
+order by user_tab_columns.table_name,user_tab_columns.column_id
+
+--创建同义词
+create or replace synonym bs_menu for t01001
+
+--同义词测试脚本
+select * from bs_menu
+
+insert into bs_menu (ID, LABEL, PARENT, ICON, COLOR) values ('002001222222222', '综合查询', '002', 'images/mainMenu/m_000.png', null);
+
+delete from bs_menu where id='002001222222222'
+
+alter table T01001 add test VARCHAR2(100);
+alter table T01001 drop column test;
+
+SELECT t.sql_text, t.last_load_time,t.*
+FROM v$sql t
+WHERE t.last_load_time IS NOT NULL
+ORDER BY t.last_load_time DESC
+
+select * from v$active_session_history 
+
+select *
+from v$sqlarea b
+where sql_text like '%1307580923%'
+--where b.FIRST_LOAD_TIME between '2009-10-15/09:24:47' and '2009-10-15/09:24:47' 
+order by b.FIRST_LOAD_TIME 
+
+select * from ys_yh_custinfo where yhid='1307580923'
+--1 B4ADF12CF98E6179E053FB3EE931ED82  kings 1307580923    0201    1 Y 李四3   武汉市武昌区xxx路        2020-11-22 15:22:37                   Y           
+select * from base_dept where dept_no='0201' and hire_code='kings'
+
+select * from ys_yh_sbdoc where sbid='1307580923'
+--1 B4ADF12CF9906179E053FB3EE931ED82  1307580923    13            2020-11-22 15:22:37                       1202011221307580923 Y N                               kings
+
+select * from ys_yh_sbinfo where yhid='1307580923'
+--1 B4ADF12CF9886179E053FB3EE931ED82  kings 1307580923  1307580923      0201  2020.11 2020.12 0200101 2               0103        01        0 2020-11-22    0       300 2020-12-1 15:38:39  300 Y             Y 4 X 12332.000   N   Y                 1                   123123      2020-11-22 15:22:37             0.00          0 0.000 0.000     Y                                                                 
+
+
